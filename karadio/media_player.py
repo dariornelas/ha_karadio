477593546e92801fb114ba37b98507e9afab9091
Dario Ornelas <dario.ornelas@tecnico.ulisboa.pt>
2019-05-09 15:50:50 +0100

    on and off working, volume set and step working, status working

--- karadio/media_player.py
+++ karadio/media_player.py
@@ -11,8 +11,8 @@ _LOGGER      = logging.getLogger(__name__)
 
 DOMAIN = "karadio"
 
-MIN_TIME_BETWEEN_SCANS = timedelta(seconds=3)
-MIN_TIME_BETWEEN_FORCED_SCANS = timedelta(seconds=1)
+MIN_TIME_BETWEEN_SCANS = timedelta(seconds=10)
+MIN_TIME_BETWEEN_FORCED_SCANS = timedelta(seconds=2)
 
 from homeassistant.helpers import config_validation as cv
 
@@ -24,9 +24,10 @@ from homeassistant.components.media_player import (
 from homeassistant.components.media_player.const import (
   MEDIA_TYPE_CHANNEL,
   SUPPORT_TURN_ON, SUPPORT_TURN_OFF,
-  SUPPORT_VOLUME_MUTE, SUPPORT_VOLUME_SET,
-  SUPPORT_PAUSE, SUPPORT_PLAY, SUPPORT_STOP
-  #SUPPORT_SELECT_SOURCE
+  SUPPORT_VOLUME_STEP, SUPPORT_VOLUME_SET,
+  SUPPORT_PAUSE, SUPPORT_PLAY, SUPPORT_STOP,
+  SUPPORT_PREVIOUS_TRACK, SUPPORT_NEXT_TRACK
+  #SUPPORT_SELECT_SOURCE SUPPORT_VOLUME_MUTE, 
 )
 
 from homeassistant.const import (
@@ -43,8 +44,9 @@ BOOL_OFF = 'off'
 BOOL_ON = 'on'
 TIMEOUT = 10
 SUPPORT_KARADIO = SUPPORT_PAUSE | SUPPORT_PLAY | SUPPORT_STOP |\
-                  SUPPORT_VOLUME_SET | SUPPORT_VOLUME_MUTE | \
-                  SUPPORT_TURN_OFF
+                  SUPPORT_VOLUME_SET | SUPPORT_VOLUME_STEP | \
+                  SUPPORT_TURN_OFF | SUPPORT_TURN_ON | \
+                  SUPPORT_NEXT_TRACK | SUPPORT_PREVIOUS_TRACK
 #                  SUPPORT_SELECT_SOURCE
 
 CONF_MAX_VOLUME = 'max_volume'
@@ -53,7 +55,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
   vol.Required(CONF_HOST, default='127.0.0.1'): cv.string,
   vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
   vol.Optional(CONF_PORT, default='23'): cv.port,
-  vol.Optional(CONF_MAX_VOLUME, default='100'): cv.string
+  vol.Optional(CONF_MAX_VOLUME, default='254'): cv.string
 })
 
 SCAN_INTERVAL = timedelta(seconds=10)
@@ -64,83 +66,81 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
   port = config.get(CONF_PORT)
   name = config.get(CONF_NAME)
   max_volume = int(config.get(CONF_MAX_VOLUME))
-  #session = _get_clientsession(hass)
   api = KaradioApi(ip, port, hass)
   add_devices([KaradioDevice(name, max_volume, api)], True)
 
 class KaradioApi():
   def __init__(self, ip, port, hass):
+    _LOGGER.info('Initializing KaradioAPI')
     self.hass = hass
     self.ip = ip
     self.port = port
-    #self.endpoint = 'http://{0}:{1}'.format(ip, port)
 
-  def _telnet_command(self, command, key):
+  def _telnet_command(self, command, key = None):
       try:
-          telnet = telnetlib.Telnet(self._resource, self._port)
-          telnet.write(command.encode('ASCII') + b'\r')
-          response = telnet.read_until(b'\r', timeout=0.2)
-          responseString = response.decode('ASCII').strip()
-          return re.findall(key,responseString)
+          if (key):
+            telnet = telnetlib.Telnet(self.ip, self.port)
+            telnet.write(command.encode('ASCII') + b'\n')
+            response = telnet.read_until(b'\r', timeout=1)
+            responseString = response.decode('ASCII').strip()
+            result = re.findall(key,responseString)
+            if (result):
+                return result[0]
+            else:
+                return None
+          else:
+            telnet = telnetlib.Telnet(self.ip, self.port)
+            telnet.write(command.encode('ASCII') + b'\n')
       except IOError as error:
           _LOGGER.error(
               'Command "%s" failed with exception: %s',
               command, repr(error))
       return None
 
-  #  def _exec_cmd(self, cmd, key_to_extract):
-  #   import xmltodict
-  #   query = urllib.parse.urlencode({ "cmd": cmd }, quote_via=urllib.parse.quote)
-  #   url = '{0}/UIC?{1}'.format(self.endpoint, query)
-
-  #   with _timeout.timeout(TIMEOUT, loop=self.hass.loop):
-  #     _LOGGER.debug("Executing: {} with cmd: {}".format(url, cmd))
-  #     response =  self.session.get(url)
-  #     data =  response.text()
-  #     _LOGGER.debug(data)
-  #     response = xmltodict.parse(data)
-  #     if key_to_extract in response['UIC']['response']:
-  #       return response['UIC']['response'][key_to_extract]
-  #     else:
-  #       return None
-
-  #  def _exec_get(self, action, key_to_extract):
-  #   return  self._exec_cmd('<name>{0}</name>'.format(action), key_to_extract)
-
-  #  def _exec_set(self, action, property_name, value):
-  #   if type(value) is str:
-  #     value_type = 'str'
-  #   else:
-  #     value_type = 'dec'
-  #   cmd = '<name>{0}</name><p type="{3}" name="{1}" val="{2}"/>'.format(action, property_name, value, value_type)
-  #   return  self._exec_cmd(cmd, property_name)
-
   def get_state(self):
-    return self._telnet_command('cli.info', '##CLI.PLAYING(.?)')
+    return  self._telnet_command('cli.info', '##CLI.ICY0#: (.*?)\n')
 
   def set_state(self, key):
-    return  self._telnet_command('SetPowerStatus', 'powerStatus', key)
+    command = "cli." + key
+    return  self._telnet_command(command)
 
   def get_main_info(self):
     return  self._telnet_command('GetMainInfo')
 
   def get_volume(self):
-    return int( self._telnet_command('cli.info', '##CLI.VOL#: (.*?)\n'))
+    return self._telnet_command('cli.vol', '##CLI.VOL#: (.*?)\n')
 
   def set_volume(self, volume):
-    return  self._telnet_command('SetVolume', 'volume', int(volume))
+    command = "cli.vol(\"" + str(volume) + "\""
+    return  self._telnet_command(command)
+
+  def volume_up(self):
+    """Volume up the media player."""
+    return  self._telnet_command('cli.vol+')
+
+  def volume_down(self):
+    """Volume down media player."""
+    return  self._telnet_command('cli.vol-')
+
+  def next_track(self):
+    """Send next track command."""
+    return  self._telnet_command('cli.next')
+
+  def previous_track(self):
+    """Send previous track command."""
+    return  self._telnet_command('cli.prev')
 
   def get_speaker_name(self):
     return  self._telnet_command('GetSpkName', 'spkname')
 
-  def get_muted(self):
-    return  self._telnet_command('GetMute', 'mute') == BOOL_ON
+  # def get_muted(self):
+  #   return  self._telnet_command('GetMute', 'mute') == BOOL_ON
 
-  def set_muted(self, mute):
-    if mute:
-      return  self._telnet_command('SetMute', 'mute', BOOL_ON)
-    else:
-      return  self._telnet_command('SetMute', 'mute', BOOL_OFF)
+  # def set_muted(self, mute):
+  #   if mute:
+  #     return  self.set_volume(0)
+  #   else:
+  #     return  self.set_volume(volume)
 
 class KaradioDevice(MediaPlayerDevice):
   def __init__(self, name, max_volume, api):
@@ -169,9 +169,9 @@ class KaradioDevice(MediaPlayerDevice):
   def volume_level(self):
     return self._volume
 
-  def _set_volume_level(self, volume):
+  def set_volume_level(self, volume):
      self.api.set_volume(volume * self._max_volume)
-     self._update()
+     self.update()
 
   # @property
   # def source(self):
@@ -185,31 +185,55 @@ class KaradioDevice(MediaPlayerDevice):
   #    self.api.set_source(source)
   #    self._update()
 
-  @property
-  def is_volume_muted(self):
-    return self._muted
+  # @property
+  # def is_volume_muted(self):
+  #   return self._muted
 
-  def _mute_volume(self, mute):
-    self._muted = mute
-    self.api.set_muted(self._muted)
-    self._update()
+  # def mute_volume(self, mute):
+  #   self._muted = mute
+  #   self.api.set_muted(self._muted)
+  #   self.update()
+
+  def volume_up(self):
+      """Volume up the media player."""
+      self.api.volume_up()
+
+  def volume_down(self):
+      """Volume down media player."""
+      self.api.volume_down()
+
+  def media_next_track(self):
+      """Send next track command."""
+      self.api.next_track()
+
+  def media_previous_track(self):
+      """Send the previous track command."""
+      self.api.previous_track()
 
   def turn_off(self):
       """Turn off media player."""
-      self.api.set_state(0)
+      self.api.set_state("stop")
 
   def turn_on(self):
       """Turn on media player."""
-      self.api.set_state(1)
+      self.api.set_state("start")
+
 
   @util.Throttle(MIN_TIME_BETWEEN_SCANS, MIN_TIME_BETWEEN_FORCED_SCANS)
-  def _update(self):
+  def update(self):
     _LOGGER.info('Refreshing state...')
     #self._current_source =  self.api.get_source()
-    self._volume =  self.api.get_volume() / self._max_volume
-    self._muted =  self.api.get_muted()
     value =  self.api.get_state()
-    if value :
+    volume = self.api.get_volume()
+    _LOGGER.info(value)
+    if (volume):
+      self._volume =  int(volume) / self._max_volume
+      # if (int(volume) == 0):
+      #   self._muted =  BOOL_ON
+      # else:
+      #   self._muted =  BOOL_OFF
+
+    if value:
       self._state = STATE_PLAYING
     else:
       self._state = STATE_OFF
